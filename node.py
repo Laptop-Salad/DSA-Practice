bf36f8f7b088b03a39b027a1a0194691d2fbc840
Laptop-Salad <email.com>
2023-03-11 02:01:50 +0000

    update node.py

--- node.py
+++ node.py
@@ -13,18 +13,3 @@ class Node:
         self.value = value
         self.next_node = next_node
 
-
-apple = Node("apple")
-banana = Node("banana")
-orange = Node("orange")
-
-apple.next_node = banana
-banana.next_node = orange
-
-# Print out all nodes
-current_node = apple
-
-while current_node is not None:
-    print(current_node.value)
-    current_node = current_node.next_node
-
