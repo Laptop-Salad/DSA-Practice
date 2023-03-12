c4ead377238c8e2153eb12e861372b6691177abc
Laptop-Salad <80591698+Laptop-Salad@users.noreply.github.com>
2023-03-12 00:26:56 +0000

    Update node.py

--- data_structures/node.py
+++ data_structures/node.py
@@ -4,9 +4,12 @@ class Node:
 
     Represents a node data structure.
 
-    Properties:
-    - value (any type) => A value for the current node
-    - next_node (Node/None) => A pointer to the next node
+    Attributes
+    ----------
+    value
+        A value for the current node
+    next_node: (Node/None) 
+        A pointer to the next node
     """
 
     def __init__(self, value, next_node=None):
